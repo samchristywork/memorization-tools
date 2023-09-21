86aa6ea5ab8dcad9ead86e19f903ec8f6b0e88c5
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-09-20 21:26:52 -0500

    feat: Add function for getting input
    This function reads standard input to get keyboard events. I think the
    longest keycode is 4 bytes.

--- src/cloze.cpp
+++ src/cloze.cpp
@@ -1,2 +1,30 @@
+#include <iostream>
+#include <string>
+#include <sys/ioctl.h>
+
+using namespace std;
+
+struct Word {
+  string content;
+  bool heldBack;
+  bool show;
+};
+
+int currentLine = 0;
+int currentWord = 0;
+string guess;
+string word;
+vector<vector<Word *> *> material;
+
+char *getInput(int *len) {
+  char *s = new char[4];
+  s[0] = 0;
+  s[1] = 0;
+  s[2] = 0;
+  s[3] = 0;
+  *len = read(STDIN_FILENO, s, 4);
+
+  return s;
+}
 void cloze(int argc, char *argv[]) {
 }
